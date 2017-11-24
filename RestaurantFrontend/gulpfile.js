edc983e217bfd54316ff3a3cb7f93cd5e5be3cc8
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2017-11-24 18:53:34 +0100

    Add gulpfile and project dependencies

--- RestaurantFrontend/gulpfile.js
+++ RestaurantFrontend/gulpfile.js
@@ -1,19 +1,43 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var useref = require('gulp-useref');
+var gulpIf = require('gulp-if');
+var runSequence = require('run-sequence');
 
-gulp.task('default', function () { 
-    console.log('Hello world!') 
+gulp.task('start', function (callback) {
+    runSequence(['less', 'useref', 'browserSync', 'watch'],
+    callback
+  )
 });
 
-gulp.task('watch', function(){
-    gulp.watch('app/less/**/*.less', ['less']); 
-    gulp.watch('app/*.html', ['html']);
-    gulp.watch('app/js/**/*.js', ['js']);
-  })
+gulp.task('watch', ['browserSync', 'less'], function () {
+    gulp.watch('app/less/**/*.less', ['less']);
+    gulp.watch('app/css/**/*.css', browserSync.reload);
+    gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+})
 
-gulp.task('less', function(){
+gulp.task('less', function () {
     return gulp.src('app/less/**/*.less')
-      .pipe(less()) // Using gulp-sass
-      .pipe(gulp.dest('app/css'))
+        .pipe(less())
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        },
+    })
+})
+
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+      .pipe(useref())
+      .pipe(gulpIf('js/**/*.js', uglify()))
+      .pipe(gulpIf('*.css', cssnano()))
+      .pipe(gulp.dest('dist'))
   });
