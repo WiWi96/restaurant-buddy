7c65df8d21be24aa30390f376b93938b71fa59d8
masxDev <maciek.sekta@gmail.com>
2017-11-24 18:29:26 +0100

    Initial commit for implementation project.

---
+++ RestaurantImpl/src/main/java/com/wmak/restaurant/impl/RestaurantImplApplication.java
@@ -0,0 +1,12 @@
+package com.wmak.restaurant.impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantImplApplication.class, args);
+	}
+}
