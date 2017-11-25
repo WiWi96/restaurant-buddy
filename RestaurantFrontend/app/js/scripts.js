a16e1c3232f3fad2618f9d27ce915f5da4274c2b
Wojciech Pszonak <wojciech.pszonak@gmail.com>
2017-11-25 09:59:49 +0100

    Add notifications

--- RestaurantFrontend/app/js/scripts.js
+++ RestaurantFrontend/app/js/scripts.js
@@ -1,3 +1,22 @@
-$(function() {
-    $("#nav").load("/nav.html");
+$(function () {
+    $("#nav").load("/nav.html", function () {
+        $("[data-toggle='popover']").popover({
+            "html": true,
+            "content": $(".notification-list").html()
+        })
+
+        $(".notification-link").on("click", function (e) {
+            $(this)
+                .find(".badge")
+                .removeClass("badge-primary")
+                .addClass("badge-secondary")
+                .html("0");
+        });
+
+        $(".notifications-dismiss").on("click", function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $(this).parent().find(".notifications").empty();
+        })
+    });
 })
